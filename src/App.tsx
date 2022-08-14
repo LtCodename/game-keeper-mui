5de4bbc42175d4bd4096be3e5df7a8b412d1c1e1
Yevhen Chernenko <chernenko.eu@gmail.com>
2022-08-14 15:29:15 +0300

    GK-002 - eslint and prettier setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <span>Game Keeper Mui</span>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <span>Game Keeper Mui</span>
+  </div>
+);
 
 export default App;
