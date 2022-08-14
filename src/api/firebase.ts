56f911c4abd7a298a75a271bca0beb55062ba4b9
Yevhen Chernenko <chernenko.eu@gmail.com>
2022-08-14 18:30:03 +0300

    GK-006 - install and init firebase

---
+++ src/api/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAupLLqr90XxEG5x_XybSGMIFtmevJgIzo",
+  authDomain: "the-game-keeper.firebaseapp.com",
+  databaseURL: "https://the-game-keeper.firebaseio.com",
+  projectId: "the-game-keeper",
+  storageBucket: "the-game-keeper.appspot.com",
+  messagingSenderId: "580632191659",
+  appId: "1:580632191659:web:786d9fd7f358c065d88dee",
+};
+
+const firebase = initializeApp(firebaseConfig);
+export default firebase;
