6c6f1fb1af16fd31ac2b16fea23bea5bfb7ad4d4
Yevhen Chernenko <chernenko.eu@gmail.com>
2022-08-17 14:18:05 +0300

    GK-017 - add sections to the store, load them from loader, get them on the list page

--- src/components/list/List.tsx
+++ src/components/list/List.tsx
@@ -4,13 +4,19 @@ import { useLocation } from "react-router-dom";
 
 import { useSelector } from "react-redux";
 
-import { IStore, IUserList } from "types";
+import { IStore, IUserList, IUserSection } from "types";
 
 const List = () => {
   const location: any = useLocation();
   const listId: string = location.pathname.replace(/[/]/, "");
+
   const userLists: IUserList[] =
     useSelector((state: IStore) => state.userLists) || [];
+
+  const userSections: IUserSection[] =
+    useSelector((state: IStore) => state.userSections).filter(
+      (section: IUserSection) => section.listId === listId
+    ) || [];
 
   useEffect(() => {
     console.log(listId);
@@ -19,6 +25,10 @@ const List = () => {
   useEffect(() => {
     console.log(userLists);
   }, [userLists]);
+
+  useEffect(() => {
+    console.log(userSections);
+  }, [userSections]);
 
   return (
     <div>
--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,14 +1,15 @@
 import { combineReducers } from "redux";
 import platformsReducer from "./platformsReducer";
 import userListsReducer from "./userListsReducer";
+import userSectionsReducer from "./userSectionsReducer";
 
 const rootReducer = combineReducers({
   platforms: platformsReducer,
   userLists: userListsReducer,
+  userSections: userSectionsReducer,
   // colors: colorsReducer.reducer,
   // userData: userReducer.reducer,
   // selectedListIndex: selectedListIndexReducer.reducer,
-  // userSections: userSectionsReducer.reducer,
   // userBlocks: userBlocksReducer.reducer,
 });
 
