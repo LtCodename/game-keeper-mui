df963bfb9044563ed4afe086538ea37e8d875783
Yevhen Chernenko <chernenko.eu@gmail.com>
2022-08-17 13:46:35 +0300

    GK-015 - move file loading to a separate component

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,72 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 import { IStore, IUserList } from "types/types";
 
 import { Outlet } from "react-router-dom";
 
-import {
-  Box,
-  Typography,
-  Stack,
-  Backdrop,
-  CircularProgress,
-} from "@mui/material/";
-
-import db from "api/firebase";
-
-import { getDoc, doc } from "@firebase/firestore";
-
-import { LISTS_SET } from "redux/actions";
+import { Box, Typography, Stack } from "@mui/material/";
 
 import ListItem from "components/list/ListItem";
 
 const Dashboard = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-
   const userLists: IUserList[] =
     useSelector((state: IStore) => state.userLists) || [];
-
-  const dispatch = useDispatch();
-
-  // const getGameInfo = () => {
-  //   getGameInformation("Firewatch")
-  //     .then((res: any) => {
-  //       console.log(res);
-  //     })
-  //     .catch((error: any) => {
-  //       console.log(error);
-  //     });
-  // };
-
-  const getData = async () => {
-    const usersRef = doc(db, "users", "f8CNiv7pLZeHe5jDmnPrO3qQAs32");
-    await getDoc(usersRef)
-      .then((doc: any) => {
-        const allUserData = doc.data() || {};
-
-        dispatch({
-          type: LISTS_SET,
-          payload: allUserData.lists,
-        });
-
-        // this.props.setSectionsToStore(allUserData.sections || []);
-        // this.props.setBlocksToStore(allUserData.blocks || []);
-      })
-      .catch((error: any) => {
-        console.log(error);
-      })
-      .finally(() => setIsLoading(false));
-  };
-
-  useEffect(() => {
-    // getGameInfo();
-    if (!userLists.length) {
-      setIsLoading(true);
-      getData(); // lists, sections, blocks
-    }
-  }, []);
 
   return (
     <Box sx={{ p: 2 }}>
@@ -80,12 +26,6 @@ const Dashboard = () => {
           </Box>
         ))}
       </Stack>
-      <Backdrop
-        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        open={isLoading}
-      >
-        <CircularProgress color="inherit" />
-      </Backdrop>
       <Outlet />
     </Box>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,25 @@
 import React from "react";
+
 import ReactDOM from "react-dom/client";
+
 import "./index.css";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Provider } from "react-redux";
+
+import { createStore, applyMiddleware } from "redux";
+
+import thunk from "redux-thunk";
+
+import rootReducer from "redux/rootReducer";
+
+import Header from "components/header/Header";
+import Loader from "components/Loader";
 import Dashboard from "components/dashboard/Dashboard";
 import List from "components/list/List";
 import Login from "components/login/Login";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "redux/rootReducer";
-import Header from "components/header/Header";
+
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
@@ -21,6 +31,7 @@ const store = createStore(rootReducer, applyMiddleware(thunk));
 root.render(
   <Provider store={store}>
     <BrowserRouter>
+      <Loader />
       <Header />
       <Routes>
         <Route path="/" element={<Dashboard />} />
