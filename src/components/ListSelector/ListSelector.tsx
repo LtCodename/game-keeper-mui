1e7628af21276cc56a543cf957ea4d65d1b01867
Yevhen Chernenko <chernenko.eu@gmail.com>
2022-08-19 16:50:21 +0300

    GK-026 - refactoring

--- src/components/ListSelector/ListSelector.tsx
+++ src/components/ListSelector/ListSelector.tsx
@@ -15,7 +15,7 @@ import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
 import ListItem from "components/List/ListItem";
-import AddListDialog from "components/AddListDialog";
+import AddListDialog from "components/AddDialogs/AddListDialog";
 
 import { IStore, IUserList } from "types";
 
